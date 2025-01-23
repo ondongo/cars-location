8bae2f3feefcf6ae04c174b9d7e10ed3b29cc348
Gloire__ODG <93401301+ondongo@users.noreply.github.com>
2025-01-23 15:14:14 +0100

    Configure Next.js image domains for external sources
    - Added 'example.com' and 'localhost:3000' to the images domain configuration in next.config.ts.
    - This change allows the application to serve images from specified external domains, enhancing image handling capabilities.
    
    This update improves the flexibility of image usage within the application.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['example.com','localhost:3000'], 
+  },
 };
 
 export default nextConfig;
